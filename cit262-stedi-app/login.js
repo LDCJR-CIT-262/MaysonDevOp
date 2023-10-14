478681b5bededb57c93f2078489b78dcb1803c46
diegomayson <zivdiego@gmail.com>
2023-10-14 18:40:53 +0000

    Just added

---
+++ cit262-stedi-app/login.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {StyleSheet, View, Text, Button, Alert} from 'react-native';
+
+export default function Login(props){
+    return(
+        <View style={styles.login}>
+            <Text>This is the Login Screen</Text>
+        
+            <Button title="Mayson" onPress={()=>{Alert.alert("Hello Mayson!!")}}></Button>
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    login: {
+        flexDirection: 'column',
+        width: '100%',
+        justifyContent: 'space-between',
+        backgroundColor: 'black',
+        height: '100',
+        paddingBottom: 5,
+        
+      },
+})
